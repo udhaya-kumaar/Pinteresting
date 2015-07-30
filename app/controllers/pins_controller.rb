1818f39a8ea5fa3546e4dcc168f7f95680e24eb1
Udhaya Kumar <akon0411@gmail.com>
2015-07-30 22:25:07 +0530

    user authentiation modified

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new]
   def index
     @pins = Pin.all
   end
@@ -9,14 +10,14 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
       if @pin.save
        redirect_to @pin, notice: 'Pin was successfully created.' 
       else
@@ -41,6 +42,11 @@ class PinsController < ApplicationController
 
   def set_pin
     @pin = Pin.find(params[:id])
+  end
+
+  def correct_user
+    @pin = current_user.pins.find_by(id: params[:id])
+    redirect_to_pins_path if @pin.nil?
   end
 
   def pin_params
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727184051) do
+ActiveRecord::Schema.define(version: 20150728171419) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
